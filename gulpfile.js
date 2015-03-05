7b29bda426f007458757162cc24b3b13e65293ef
frederic <frederic@lust.nl>
2015-03-05 15:11:32 +0100

    documentation

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,35 @@ var rename = require('gulp-rename');
 var mocha = require('gulp-mocha');
 var notifier = require('node-notifier');
 var path = require('path');
+var docco = require('gulp-docco');
+var changed = require('gulp-changed');
+var clean = require('gulp-clean');
 
 var name = 'trespass';
-var src_dir = 'src';
-var output_dir = 'dist';
+var src_dir = './src';
+var output_dir = './dist';
 var main_filename = 'index.js';
 var main_filepath = path.join(src_dir, main_filename);
 var source_file_pattern = path.join(src_dir, '**/*.js');
-var test_dir = 'test';
+var test_dir = './test';
 var test_file_pattern = path.join(test_dir, '*.js');
+var docs_dir = './docs';
+var docs_file_pattern = source_file_pattern;
+
+
+gulp.task('clean-docco', function() {
+	return gulp.src(docs_dir, { read: false })
+		.pipe(clean());
+});
+
+gulp.task('docco', /*['clean-docco'],*/ function() {
+	return gulp.src(docs_file_pattern)
+		.pipe(changed(docs_dir)) // incremental build
+		.pipe(docco({
+			// css: 'path/to/custom/docco.css'
+		}))
+		.pipe(gulp.dest(docs_dir))
+});
 
 
 gulp.task('mocha', function() {
@@ -32,7 +52,7 @@ gulp.task('webpack', function() {
 		.pipe(webpack({
 			watch: true,
 			output: {
-				library: name
+				// library: name // standalone
 			}
 		}))
 		.pipe(rename({
@@ -42,6 +62,11 @@ gulp.task('webpack', function() {
 });
 
 
-gulp.task('default', ['mocha', 'webpack'], function() {
-	gulp.watch([source_file_pattern], ['mocha']);
+gulp.task('watch', function() {
+	gulp.watch([source_file_pattern, test_file_pattern], ['mocha']);
+	gulp.watch(source_file_pattern, ['docco']);
 });
+
+
+gulp.task('default', ['mocha', 'watch'/*, 'webpack'*/]);
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
+// `trespass.js` is a javascript library for the TREsPASS project.
+// it consists of the following sub-modules:
 
 module.exports = {
-	hello: 'world'
+	// ## [trespass.placeholder](placeholder.js.html)
+	// description ...
+	/*placeholder: require('./placeholder.js')*/
 };
