460dd7ef5c17843eb67a0e60de808d2ba7761e90
frederic <frederic@lust.nl>
2015-04-01 13:51:03 +0200

    formatting test output

--- test/test.js
+++ test/test.js
@@ -6,26 +6,33 @@ var libxml = require('libxmljs');
 
 var root_dir = path.join(__dirname, '..');
 
+var f1 = function(s) {
+	return chalk.magenta(s);
+};
+var f2 = function(s) {
+	return chalk.bgMagenta.black(s);
+};
+var f3 = function(s) {
+	return chalk.bgMagenta.white(s);
+};
 
-function higlight(s) {
-	return chalk.bgBlue(s);
-}
 
+describe(f1('validation'), function() {
 
-describe(higlight('validation'), function() {
+	describe(f2('model'), function() {
+		it(f3('should be a valid document'), function() {
+			var schema_content = fs.readFileSync(
+				path.join(root_dir, 'data', 'TREsPASS_model.xsd')
+			).toString();
+			var schema = libxml.parseXmlString(schema_content);
 
-	it('model should be a valid document', function() {
-		var schema_content = fs.readFileSync(
-			path.join(root_dir, 'data', 'TREsPASS_model.xsd')
-		).toString();
-		var schema = libxml.parseXmlString(schema_content);
+			var model_content = fs.readFileSync(
+				path.join(root_dir, 'data', 'model_cloud_review.xml')
+			).toString();
+			var model = libxml.parseXmlString(model_content);
 
-		var model_content = fs.readFileSync(
-			path.join(root_dir, 'data', 'model_cloud_review.xml')
-		).toString();
-		var model = libxml.parseXmlString(model_content);
-
-		assert.equal(model.validate(schema), true);
+			assert.equal(model.validate(schema), true);
+		});
 	});
 
 });
