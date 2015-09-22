f55ef75ae4b5061929601801414449c8bb36305e
freder <freder.code@gmail.com>
2015-09-22 16:36:26 +0200

    updated description of submodules + renamed tree to attacktree

--- src/trespass.js
+++ src/trespass.js
@@ -1,17 +1,16 @@
 'use strict';
 
-// `trespass.js` is a javascript library for the [TREsPASS project](https://www.trespass-project.eu/).
-// it consists of the following sub-modules:
+// `trespass.js` is the core library used in the node.js or web-based tools in the [TREsPASS project](https://www.trespass-project.eu/).
+// it is divided into the following sub-modules:
 
 module.exports = {
 	// ## [trespass.model](model.html)
 	// functions to work with the trespass model. Can load models in the xml format, or create models from scratch. also provides conversion from the internal representation back to xml.
-	// processing the socio-technical model XML format.
 	model: require('./model.js'),
 
-	// ## [trespass.tree](tree.html)
+	// ## [trespass.attacktree](attacktree.html)
 	// functions to process attack trees — including loading, doing basic calculations on them, and preparing them for visualization.
-	tree: require('./tree.js'),
+	attacktree: require('./attacktree.js'),
 
 	// ## [trespass.util](util.html)
 	// utility functions for manipulating xml dom nodes, and other common helper functions.
