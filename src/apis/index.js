fe92f7b5f92b9f97894d477809e178904ef1c49b
freder <freder.code@gmail.com>
2016-02-03 14:12:32 +0100

    more api code

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,11 +1,34 @@
 'use strict';
 
 
+const makeUrl = module.exports.makeUrl =
+function makeUrl(api, endpoint) {
+	return api.host + api.prefix + endpoint;
+};
+
+
+// jquery request options
+const requestOptions = module.exports.requestOptions = {
+	crossDomain: {
+		crossDomain: true,
+		xhrFields: { withCredentials: true }, // to send session cookie
+	},
+
+	fileUpload: {
+		type: 'POST',
+		processData: false,
+		contentType: false,
+	}
+};
+
+
 /*
 itrust tools API
 https://trespass.itrust.lu/api/json
 */
-module.exports.tools = {
-	host: 'https://trespass.itrust.lu/',
-	prefix: 'api/json/',
+module.exports.apis = {
+	tools: {
+		host: 'https://trespass.itrust.lu/',
+		prefix: 'api/json/',
+	}
 };
