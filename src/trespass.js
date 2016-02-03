6ea98d36702a3d2bd1e79207bb34d63b48e9dbd9
freder <freder.code@gmail.com>
2016-02-03 10:11:16 +0100

    removed util, added apis

--- src/trespass.js
+++ src/trespass.js
@@ -1,18 +1,18 @@
 'use strict';
 
 // `trespass.js` is the core library used in the node.js or web-based tools in the [TREsPASS project](https://www.trespass-project.eu/).
-// it is divided into the following sub-modules:
+// It is divided into the following sub-modules:
 
 module.exports = {
 	// ## [trespass.model](model.html)
-	// functions to work with the trespass model. Can load models in the xml format, or create models from scratch. also provides conversion from the internal representation back to xml.
+	// Functions to work with the TREsPASS model format.
 	model: require('./model.js'),
 
 	// ## [trespass.attacktree](attacktree.html)
-	// functions to process attack trees — including loading, doing basic calculations on them, and preparing them for visualization.
+	// Functions to process attack trees.
 	attacktree: require('./attacktree.js'),
 
-	// ## [trespass.util](util.html)
-	// utility functions for manipulating xml dom nodes, and other common helper functions.
-	util: require('./util.js'),
+	// ## [trespass.apis](apis.html)
+	// Functions for using a number of APIs.
+	apis: require('./apis.js'),
 };
