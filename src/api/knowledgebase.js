52650e7fd8d1777067769a7090882bc05b8cfcc6
freder <freder.code@gmail.com>
2016-05-03 16:35:49 +0200

    use different host address on itrust server

--- src/api/knowledgebase.js
+++ src/api/knowledgebase.js
@@ -9,7 +9,9 @@ const api = require('./index.js');
 knowledge base API
 http://localhost:8080/tkb/
 */
-const host = module.exports.host = 'http://0.0.0.0:8080/';
+const host = module.exports.host = (window.location.toString().indexOf('itrust') > -1)
+	? 'https://trespass-tkb.itrust.lu/'
+	: 'http://localhost:8080/';
 const prefix = module.exports.prefix = 'tkb/';
 const paths = { host, prefix };
 // ———
