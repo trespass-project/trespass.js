d760662c4620a41b91e9547c0826d8aa976471c8
freder <freder.code@gmail.com>
2016-06-24 14:45:31 +0200

    linting

--- test/test.js
+++ test/test.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const assert = require('assert');
 const R = require('ramda');
 const _ = require('lodash');
@@ -25,15 +23,9 @@ const cheerioOptions = {
 
 const trespass = require('../');
 
-const f1 = function(s) {
-	return chalk.magenta(s);
-};
-const f2 = function(s) {
-	return chalk.bgMagenta.black(s);
-};
-const f3 = function(s) {
-	return chalk.bgMagenta.white(s);
-};
+const f1 = (s) => chalk.magenta(s);
+const f2 = (s) => chalk.bgMagenta.black(s);
+const f3 = (s) => chalk.bgMagenta.white(s);
 
 
 const validateXML = require('xmllint').validateXML;
@@ -48,10 +40,10 @@ function validateXmlWithSchema(schemaStr, modelStr) {
 }
 
 
-// describe(f1('validation'), function() {
+// describe(f1('validation'), () => {
 
-// 	describe(f2('model'), function() {
-// 		it(f3('should be a valid document'), function() {
+// 	describe(f2('model'), () => {
+// 		it(f3('should be a valid document'), () => {
 // 			const schemaContent = fs.readFileSync(
 // 				path.join(rootDir, 'data', 'TREsPASS_model.xsd')
 // 			).toString();
@@ -70,7 +62,7 @@ function validateXmlWithSchema(schemaStr, modelStr) {
 
 
 // ---
-describe(f1('trespass.model'), function() {
+describe(f1('trespass.model'), () => {
 	// describe(f2('test file'), () => {
 	// 	it(f3('should be valid'), () => {
 	// 		const isValid = validateXmlWithSchema(schemaStr, testModelXML);
@@ -80,28 +72,28 @@ describe(f1('trespass.model'), function() {
 
 	describe(f2('.parse()'), () => {
 		const newModel = trespass.model.create();
-		it(f3('should dynamically create empty collections'), function() {
+		it(f3('should dynamically create empty collections'), () => {
 			assert(newModel.system.locations.length === 0);
 			assert(newModel.system.data.length === 0);
 			assert(newModel.system.items.length === 0);
 		});
 	});
 
-	describe(f2('.parse()'), function() {
-		trespass.model.parse(testModelXML, function(err, model) {
-			it(f3('should import metadata'), function(done) {
+	describe(f2('.parse()'), () => {
+		trespass.model.parse(testModelXML, (err, model) => {
+			it(f3('should import metadata'), (done) => {
 				assert(model.system.author === 'ciab-exportAsTML.py');
 				assert(model.system.version === '0.5');
 				// assert(model.system.date === '2016-01-17T23:20:21.866232');
 				done();
 			});
 
-			it(f3('should import title'), function(done) {
+			it(f3('should import title'), (done) => {
 				assert(model.system.title === 'CIAB-created TREsPASS XML model');
 				done();
 			});
 
-			it(f3('should import rest of model'), function(done) {
+			it(f3('should import rest of model'), (done) => {
 				const predicates = model.system.predicates;
 				assert(predicates.length === 3);
 				assert(predicates[0].value.length === 26);
@@ -111,13 +103,13 @@ describe(f1('trespass.model'), function() {
 				done();
 			});
 
-			it(f3('atLocations should always be an Array'), function(done) {
+			it(f3('atLocations should always be an Array'), (done) => {
 				const data = model.system.data;
 				assert(_.isArray(data[0].atLocations));
 				done();
 			});
 
-			it(f3('predicate value should always be an Array'), function(done) {
+			it(f3('predicate value should always be an Array'), (done) => {
 				const predicates = model.system.predicates;
 				// console.log(predicates[0].value[0]);
 				assert(_.isArray(predicates[0].value[0]));
@@ -128,8 +120,8 @@ describe(f1('trespass.model'), function() {
 		let modelXML = fs.readFileSync(
 			path.join(rootDir, 'test', 'data', 'test.xml')
 		).toString();
-		trespass.model.parse(modelXML, function(err, model) {
-			it(f3('should not produce any weird [undefined]s'), function(done) {
+		trespass.model.parse(modelXML, (err, model) => {
+			it(f3('should not produce any weird [undefined]s'), (done) => {
 				assert(!model.system.items || model.system.items.length === 0);
 				done();
 			});
@@ -138,8 +130,8 @@ describe(f1('trespass.model'), function() {
 		modelXML = fs.readFileSync(
 			path.join(rootDir, 'test', 'data', 'anm-data.xml')
 		).toString();
-		trespass.model.parse(modelXML, function(err, model) {
-			it(f3('should import and parse ANM data'), function(done) {
+		trespass.model.parse(modelXML, (err, model) => {
+			it(f3('should import and parse ANM data'), (done) => {
 				// console.log(model.system.anm_data.system);
 				assert(!!model.system.anm_data.system);
 				assert(model.system.anm_data.system.title === 'embedded');
@@ -149,42 +141,42 @@ describe(f1('trespass.model'), function() {
 		});
 	});
 
-	describe(f2('.add*()'), function() {
+	describe(f2('.add*()'), () => {
 		let model = trespass.model.create();
 		const atLocation = 'atLocation';
 
-		it(f3('should create rooms as locations'), function() {
+		it(f3('should create rooms as locations'), () => {
 			model = trespass.model.addRoom(model, { id: 'test-room' });
-			assert( model.system.locations.length === 1 );
+			assert(model.system.locations.length === 1);
 		});
 
-		it(f3('should create rooms atLocations'), function() {
+		it(f3('should create rooms atLocations'), () => {
 			model = trespass.model.addRoom(model, {
 				id: 'test-room-2',
 				atLocations: [atLocation]
 			});
-			assert( model.system.locations.length === 2 );
-			assert( model.system.locations[1].atLocations[0] === atLocation );
+			assert(model.system.locations.length === 2);
+			assert(model.system.locations[1].atLocations[0] === atLocation);
 		});
 
-		it(f3('should create actors'), function() {
+		it(f3('should create actors'), () => {
 			model = trespass.model.addActor(model, {
 				id: 'an-actor',
 				atLocations: [atLocation]
 			});
-			assert( model.system.actors.length === 1 );
+			assert(model.system.actors.length === 1);
 		});
 
 		// TODO: more?
 
 		// TODO: turn this on again
-		// it(f3('should validate input'), function() {
-		// 	assert.throws(function() {
+		// it(f3('should validate input'), () => {
+		// 	assert.throws(() => {
 		// 		model = trespass.model.addRoom(model, {
 		// 			// missing `id`
 		// 		});
 		// 	});
-		// 	assert.throws(function() {
+		// 	assert.throws(() => {
 		// 		model = trespass.model.addRoom(model, {
 		// 			domain: '!@#$'
 		// 		});
@@ -192,19 +184,19 @@ describe(f1('trespass.model'), function() {
 		// });
 	});
 
-	describe(f2('.singular()'), function() {
-		it(f3('should return known singular'), function() {
+	describe(f2('.singular()'), () => {
+		it(f3('should return known singular'), () => {
 			const s = trespass.model.singular('policies');
 			assert(s === 'policy');
 		});
 
-		it(f3('should return `undefined`, if unknonw'), function() {
+		it(f3('should return `undefined`, if unknonw'), () => {
 			const s = trespass.model.singular('hamburgers');
 			assert(s === undefined);
 		});
 	});
 
-	describe(f2('.separateAttributeFromObject()'), function() {
+	describe(f2('.separateAttributeFromObject()'), () => {
 		const attributes = ['attr1', 'attr2'];
 		const obj = {
 			'attr1': 'attr1',
@@ -215,11 +207,11 @@ describe(f1('trespass.model'), function() {
 		const newObject = separated.newObject;
 		const attrObject = separated.attrObject;
 
-		it(f3('should return 2 objects'), function() {
+		it(f3('should return 2 objects'), () => {
 			assert(!!newObject && !!attrObject);
 		});
 
-		it(f3('should sort field correctly'), function() {
+		it(f3('should sort field correctly'), () => {
 			assert(R.keys(newObject).length === 1);
 			assert(R.keys(attrObject).length === 2);
 			assert(R.equals(attributes, R.keys(attrObject)));
@@ -228,8 +220,8 @@ describe(f1('trespass.model'), function() {
 		});
 	});
 
-	describe(f2('.prepareModelForXml()'), function() {
-		it(f3('should prefix all the elements of known root-level collections'), function() {
+	describe(f2('.prepareModelForXml()'), () => {
+		it(f3('should prefix all the elements of known root-level collections'), () => {
 			const system = {
 				locations: [
 					{ id: 'location-id-1' },
@@ -251,26 +243,26 @@ describe(f1('trespass.model'), function() {
 			assert(model.system.unknowns.length === 2);
 
 			model.system.locations.location
-				.forEach(function(item) {
+				.forEach((item) => {
 					const keys = R.keys(item);
 					assert(keys.length === 1);
 					assert(keys[0] === 'id');
 				});
 			model.system.actors.actor
-				.forEach(function(item) {
+				.forEach((item) => {
 					const keys = R.keys(item);
 					assert(keys.length === 1);
 					assert(keys[0] === 'id');
 				});
 			model.system.unknowns
-				.forEach(function(item) {
+				.forEach((item) => {
 					const keys = R.keys(item);
 					assert(keys.length === 1);
 					assert(keys[0] === 'id');
 				});
 		});
 
-		it(f3('should join items and data in assets'), function() {
+		it(f3('should join items and data in assets'), () => {
 			const system = {
 				items: [
 					{ id: 'item-id-1' },
@@ -293,8 +285,8 @@ describe(f1('trespass.model'), function() {
 		});
 	});
 
-	describe(f2('.prepareForXml()'), function() {
-		it(f3('should leave literals as they are'), function() {
+	describe(f2('.prepareForXml()'), () => {
+		it(f3('should leave literals as they are'), () => {
 			const data = {
 				version: 0.1,
 				author: 'author name',
@@ -305,7 +297,7 @@ describe(f1('trespass.model'), function() {
 			assert(preparedData.author === 'author name');
 		});
 
-		// it(f3('should work with arbitrarily nested elements'), function() {
+		// it(f3('should work with arbitrarily nested elements'), () => {
 		// 	let data = {
 		// 		one: {
 		// 			two: {
@@ -321,7 +313,7 @@ describe(f1('trespass.model'), function() {
 		// 	assert(data[0]['one'][0]['two'][0]['three'] === 'value');
 		// });
 
-		it(f3('should join atLocations'), function() {
+		it(f3('should join atLocations'), () => {
 			const data = {
 				atLocations: ['atLocation-1', 'atLocation-2']
 			};
@@ -329,7 +321,7 @@ describe(f1('trespass.model'), function() {
 			assert(preparedData.atLocations === 'atLocation-1 atLocation-2');
 		});
 
-		it(f3('should create attributes'), function() {
+		it(f3('should create attributes'), () => {
 			const data = {
 				system: {
 					date: 'date',
@@ -352,7 +344,7 @@ describe(f1('trespass.model'), function() {
 			assert(preparedData.system.locations.location[0][attrKey].id === 'location');
 		});
 
-		it(f3('should work'), function() {
+		it(f3('should work'), () => {
 			const model = {
 				system: {
 					predicates: [
@@ -375,7 +367,7 @@ describe(f1('trespass.model'), function() {
 		// TODO: what else?
 	});
 
-	describe(f2('.toXML()'), function() {
+	describe(f2('.toXML()'), () => {
 		const origModel = {
 			system: {
 				id: 'model-id',
@@ -396,22 +388,22 @@ describe(f1('trespass.model'), function() {
 		const xmlStr = trespass.model.toXML(origModel);
 		const $system = cheerio.load(xmlStr, cheerioOptions)('system');
 
-		it(f3('should properly transform model object to XML'), function() {
-			assert( $system.find('locations > location').length === 2 );
-			assert( $system.find('predicates > predicate').length === 1 );
-			assert( $system.find('atLocations').length === 1 );
+		it(f3('should properly transform model object to XML'), () => {
+			assert($system.find('locations > location').length === 2);
+			assert($system.find('predicates > predicate').length === 1);
+			assert($system.find('atLocations').length === 1);
 		});
 
-		it(f3('should remove empty collections'), function() {
-			assert( $system.find('actors').length === 0 );
-			assert( $system.find('assets').length === 0 );
+		it(f3('should remove empty collections'), () => {
+			assert($system.find('actors').length === 0);
+			assert($system.find('assets').length === 0);
 		});
 
-		it(f3('should not make elements from array items'), function() {
-			assert( xmlStr.indexOf('<0>') === -1 );
+		it(f3('should not make elements from array items'), () => {
+			assert(xmlStr.indexOf('<0>') === -1);
 		});
 
-		it(f3('should require an id'), function() {
+		it(f3('should require an id'), () => {
 			const origModel = {
 				system: {
 					id: undefined,
@@ -428,9 +420,9 @@ describe(f1('trespass.model'), function() {
 		// 	assert(isValid);
 		// });
 
-		it(f3('should re-import model successfully'), function(done) {
-			trespass.model.parse(xmlStr, function(err, model) {
-				assert(model.system.locations.length === origModel.system.locations.length );
+		it(f3('should re-import model successfully'), (done) => {
+			trespass.model.parse(xmlStr, (err, model) => {
+				assert(model.system.locations.length === origModel.system.locations.length);
 				assert(model.system.locations[1].id === 'location-2');
 				assert(model.system.locations[1].atLocations.length === 2);
 				assert(model.system.locations[1].atLocations[0] === 'loc-1');
@@ -446,21 +438,21 @@ describe(f1('trespass.model'), function() {
 			});
 		});
 
-		it(f3('test file model should be equal to export-imported model'), function(done) {
+		it(f3('test file model should be equal to export-imported model'), (done) => {
 			// import test file
 			// export it as xml
 			// import exported xml
 			// then compare both imported models
 
-			trespass.model.parse(testModelXML, function(err, model) {
+			trespass.model.parse(testModelXML, (err, model) => {
 				// console.log( JSON.stringify(model) );
 
 				const xmlStr2 = trespass.model.toXML(model);
 				// console.log(xmlStr2);
 				// console.log(xmlStr2.indexOf('<0>'));
-				// assert( xmlStr2.indexOf('<0>') === -1 );
+				// assert(xmlStr2.indexOf('<0>') === -1);
 
-				trespass.model.parse(xmlStr2, function(err, model2) {
+				trespass.model.parse(xmlStr2, (err, model2) => {
 					// console.log(xmlStr2);
 
 					const differences = diff(model, model2);
@@ -481,29 +473,30 @@ describe(f1('trespass.model'), function() {
 	// ——————————————————————————————————————
 	// SCENARIO
 
-	describe(f2('.scenarioSetModel()'), function() {
+	describe(f2('.scenarioSetModel()'), () => {
 		const empty = trespass.model.createScenario();
 		const scenario = trespass.model.scenarioSetModel(empty, 'model-file-name.xml');
 
-		it(f3('should add model'), function() {
+		it(f3('should add model'), () => {
 			assert(!!scenario.scenario.model);
 			assert(scenario.scenario.model === 'model-file-name.xml');
 			assert(scenario.scenario.model !== empty.scenario.model);
 		});
 	});
 
-	describe(f2('.scenarioSetAssetGoal()'), function() {
+	describe(f2('.scenarioSetAssetGoal()'), () => {
 		const empty = trespass.model.createScenario();
 		const scenario = trespass.model.scenarioSetAssetGoal(empty, 'attackerId', 'assetId');
 		// console.log(scenario);
 
-		it(f3('should add goal'), function() {
+		it(f3('should add goal'), () => {
 			assert(!!scenario.scenario.assetGoal);
 			assert(scenario.scenario.assetGoal.attacker === 'attackerId');
 			assert(scenario.scenario.assetGoal.asset === 'assetId');
 		});
+	});
 
-	describe(f2('.scenarioToXML()'), function() {
+	describe(f2('.scenarioToXML()'), () => {
 		it(f3('should require an id'), () => {
 			const origScenario = {
 				scenario: { id: undefined }
@@ -521,19 +514,17 @@ describe(f1('trespass.model'), function() {
 		const xmlStr = trespass.model.scenarioToXML(scenario);
 		const $system = cheerio.load(xmlStr, cheerioOptions)('scenario');
 
-		it(f3('should properly transform scenario object to XML'), function() {
-			assert( $system.find('model').text() === 'model-file-name.xml' );
-			assert( $system.find('assetGoal').attr('attacker') === 'attackerId' );
-			assert( $system.find('assetGoal > asset').text() === 'assetId' );
+		it(f3('should properly transform scenario object to XML'), () => {
+			assert($system.find('model').text() === 'model-file-name.xml');
+			assert($system.find('assetGoal').attr('attacker') === 'attackerId');
+			assert($system.find('assetGoal > asset').text() === 'assetId');
 		});
 	});
 
 	// ——————————————————————————————————————
 
-	});
-
-	describe(f2('.validateComponent()'), function() {
-		it(f3('should work'), function() {
+	describe(f2('.validateComponent()'), () => {
+		it(f3('should work'), () => {
 			let obj = { type: 'type', };
 			let result = trespass.model.validateComponent(obj, 'location');
 			assert(result.length);
@@ -544,7 +535,7 @@ describe(f1('trespass.model'), function() {
 			assert(result.length === 2);
 		});
 
-		it(f3('should work with arrays of things'), function() {
+		it(f3('should work with arrays of things'), () => {
 			let obj = {
 				id: 'actor',
 				atLocations: [],
@@ -560,12 +551,12 @@ describe(f1('trespass.model'), function() {
 			assert(result.length > 0);
 		});
 
-		it(f3('should return a custom message for atLocations'), function() {
-			let obj = {
+		it(f3('should return a custom message for atLocations'), () => {
+			const obj = {
 				id: 'actor',
 				// atLocations: [],
 			};
-			let result = trespass.model.validateComponent(obj, 'actor');
+			const result = trespass.model.validateComponent(obj, 'actor');
 			assert(result[0].message === 'actor must be located somewhere');
 		});
 	});
