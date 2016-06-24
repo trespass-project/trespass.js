317d5c7e11a613e5f5b123e5975f3815c532e650
freder <freder.code@gmail.com>
2016-06-24 14:47:52 +0200

    function to find leaf nodes

--- src/attacktree/index.js
+++ src/attacktree/index.js
@@ -1,1 +1,17 @@
-'use strict';
+const findLeafNodes =
+module.exports.findLeafNodes =
+function findLeafNodes(nodes, childrenKey='node') {
+	const leafNodes = [];
+
+	function recurse(item) {
+		const isLeaf = !item[childrenKey];
+		if (isLeaf) {
+			leafNodes.push(item);
+		} else {
+			item[childrenKey].forEach(node => recurse(node));
+		}
+	}
+
+	nodes.forEach(node => recurse(node));
+	return leafNodes;
+};
--- test/test.js
+++ test/test.js
@@ -561,3 +561,44 @@ describe(f1('trespass.model'), () => {
 		});
 	});
 });
+
+
+describe(f1('trespass.attacktree'), () => {
+	describe(f2('.findLeafNodes()'), () => {
+		it(f3('should find all leaf nodes'), () => {
+			const NOT_A_LEAF = 'not-a-leaf-node';
+			const tree = {
+				label: NOT_A_LEAF,
+				node: [
+					{
+						label: NOT_A_LEAF,
+						node: [{ label: 'leaf-1' }]
+					},
+					{
+						label: NOT_A_LEAF,
+						node: [
+							{ label: 'leaf-2' },
+							{ label: 'leaf-3' },
+						]
+					},
+					{
+						label: NOT_A_LEAF,
+						node: [
+							{
+								label: NOT_A_LEAF,
+								node: [
+									{ label: 'leaf-4' },
+								]
+							},
+							{ label: 'leaf-5' },
+						]
+					},
+				]
+			};
+			const leafNodes = trespass.attacktree.findLeafNodes([tree]);
+			assert(leafNodes.length === 5);
+			const labels = leafNodes.map(R.prop('label'));
+			assert(!R.contains(NOT_A_LEAF, labels));
+		});
+	});
+});
